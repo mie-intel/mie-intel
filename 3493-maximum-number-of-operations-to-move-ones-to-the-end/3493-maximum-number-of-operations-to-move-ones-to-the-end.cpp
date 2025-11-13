63d4d6fcd706bd68b3a7e579e4ce61451b9da352
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-11-13 07:34:02 +0700

    Time: 2 ms (86.23%), Space: 15.8 MB (75%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -4,15 +4,13 @@ public:
         int ones = 0;
         int last = -1;
         int ans = 0;
-        for(int i = 0; i < s.length(); ++i){
-            if(s[i] == '1'){
+        for(int i = 0; i <= s.length(); ++i){
+            if(i == s.length() || s[i] == '1'){
                 ans += (last != -1 && i-last > 1) * ones;
                 last = i;
                 ones++;
             }
         }
-        int n = s.length();
-        ans += (last != -1 && n-last > 1) * ones;
         return ans;
     }
 };
