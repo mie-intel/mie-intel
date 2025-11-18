5ecc4449d573f846e341f710776717536c8a14d5
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-11-18 22:37:52 +0700

    Time: 0 ms (100%), Space: 13.1 MB (41.51%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -4,8 +4,7 @@ public:
         int n = bits.size();
         for(int i = 0; i < n;){
             if(i == n - 1) return true;
-            if(bits[i] == 1) i += 2;
-            else i++;
+            i += (bits[i] + 1);
         }
         return false;
     }
