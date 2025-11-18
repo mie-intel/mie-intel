dcf3d9b08c8e2ea5375f46a8e20222b0b70136ad
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-11-18 22:37:22 +0700

    Time: 0 ms (100%), Space: 13.2 MB (8.09%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        for(int i = 0; i < n;){
+            if(i == n - 1) return true;
+            if(bits[i] == 1) i += 2;
+            else i++;
+        }
+        return false;
+    }
+};
